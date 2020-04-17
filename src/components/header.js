9f64054a48110e86254bf4f5b92eabba90a2696e
Philippos Tsamantanis <philip.tsamantanis@icloud.com>
2020-04-17 20:36:17 +0300

    Initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -3,31 +3,12 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+    <header className="header">
+        <div className="header-styles text-center">
+            <h1 className="m-auto text-white">
+                {siteTitle}
+            </h1>
+        </div>
   </header>
 )
 
@@ -36,7 +17,7 @@ Header.propTypes = {
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: 'Howl For Freedom',
 }
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,116 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+    <Layout>
+        <SEO title="Howl For Freedom" />
+        <div className="text-center width-responsive">
+            <p>Recently I attended a graduation ceremony</p>
+            <p>An exciting event, and the industry's most valuable sanctimony.</p>
+            <p>Present the alumni, and the dean,</p>
+            <p>present the students, same routine,</p>
+            <p>religious advisers and honorary members,</p>
+            <p>a few more important names that nobody remembers.</p>
+
+            <br />
+            <br />
+
+            <p>Lets give them all a few minutes each;</p>
+            <p>just to hear the same old speech.</p>
+            <p>"You're the future of our well organized society!"</p>
+            <p>Most speakers just promote reasoning-sobriety</p>
+            <p>"You're the people who will change the world"</p>
+            <p>the world of death and chaos, where your voice is not being heard.</p>
+            <p>Tell you how it is important to stay creative, because it separates us from machines</p>
+            <p>while java has become your native, and all the day you look at screens.</p>
+            <p>More inventions, resources, and industries to evolve in</p>
+            <p>More material goods for all our brains to dissolve in.</p>
+
+            <br />
+            <br />
+
+            <p>Your whole persistance in a string of bits</p>
+            <p>and your life's earnings, composed of money pits.</p>
+            <p>Inherent need to hide the State of Nature,</p>
+            <p>presented by the media, as the best artist's portraiture.</p>
+            <p>A world filled with chaos, death, and deceit</p>
+            <p>"I care about the hungry! Didn't you see my tweet?"</p>
+            <p>all a result of an ideological drought</p>
+            <p>Isn't that smart home, something you could live without?</p>
+
+            <br />
+            <br />
+
+            <p>Your whole existance in a serial number</p>
+            <p>and your conciousness, their artificial slumber.</p>
+            <p>Spending all your life waiting in a line</p>
+            <p>to buy the fancy new TV with the new improved design,</p>
+            <p>a new smartphone with so much to gain,</p>
+            <p>tell me, is it your phone, or your whole brain?</p>
+
+            <br />
+            <br />
+
+            <p>A small device with all your private information,</p>
+            <p>the strongest contributing factor to your mind's sedation.</p>
+            <p>We have accepted every term and every condition</p>
+            <p>and have provided them with enough ammunition</p>
+            <p>to shoot our privacy right in the face</p>
+            <p>and use our information to put presidents in place.</p>
+
+            <br />
+            <br />
+
+            <p>We voted for them without realizing</p>
+            <p>all according to the plan they were devising</p>
+            <p>Not with our preset elections</p>
+            <p>but with our constant material collections</p>
+            <p>Which placed law above morals, and money above the law</p>
+            <p>A monsterous creature, that we cannot declaw.</p>
+            <p>A ceaseless cycle we cannot escape</p>
+            <p>An "infinite loop of hell", on repeat, on a tape.</p>
+
+            <br />
+            <br />
+
+            <p>Well I refuse all your manipulation,</p>
+            <p>the false knowledge, the fake news, and the shitty manifestation!</p>
+
+            <br />
+            <br />
+
+            <p>They keep on saying "Make the world a better place"</p>
+            <p>That's not even possible, during a money-making race!</p>
+            <p>There needs to be improvement in general ideology!</p>
+            <p>More than religion, business, politics, HELL, even technology.</p>
+            <p>We need to start reforming, the ways we treat each other.</p>
+            <p>We need to stand up and act, we need to pother.</p>
+
+            <br />
+            <br />
+
+            <p>I suggest that we all seek,</p>
+            <p>a world composed of reason, and critique</p>
+            <p>where people don't sit numb for those who oppress</p>
+            <p>or measure success by the shitload they possess.</p>
+            <p>I seek a world where people feel,</p>
+            <p>where people shout, but never squeal</p>
+            <p>a world where voices can be heard</p>
+            <p>where our perception is not blurred.</p>
+
+            { /* You might say i'm going crazy,
+            that i've lost control, and that my thoughts are hazey.
+            Well my mind's control is hard to obtain,
+            but so is a society without sufferring nor pain.*/}
+
+            <br />
+            <br />
+
+            <p>This is a call for those who are still awake</p>
+            <p>those who wish, peace to undertake.</p>
+            <p>Turn off your screens and go out on the street,</p>
+            <p>go <strong>howl for freedom</strong>, or accept defeat.</p>
+        </div>
+    </Layout>
+);
 
 export default IndexPage
